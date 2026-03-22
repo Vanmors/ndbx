b3ccaff6539fe7bf6b287e30a4de08a8a0fbab2e
imorikov <imorikov@solanteq.com>
2026-03-22 22:54:51 +0300

    Fixed mvn install

--- src/main/java/com/vanmors/ndbx/entity/User.java
+++ src/main/java/com/vanmors/ndbx/entity/User.java
@@ -1,13 +1,11 @@
 package com.vanmors.ndbx.entity;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
 
-@Data
 @Document(collection = "users")
 public class User {
     public String getId() {

14395fb8d398cddb4c772ecfe7f06048e341da23
imorikov <imorikov@solanteq.com>
2026-03-24 23:16:17 +0300

    Fixed dependencies

--- src/main/java/com/vanmors/ndbx/config/SecurityConfig.java
+++ src/main/java/com/vanmors/ndbx/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.vanmors.ndbx.config;
 
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.SerializationFeature;
 
 
 @Configuration

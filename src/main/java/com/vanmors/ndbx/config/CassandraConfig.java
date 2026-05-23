eba10cab5d7acb53f233fda7dbb679d9a1d521df
imorikov <imorikov@solanteq.com>
2026-05-23 12:01:45 +0300

    Added  cassandra-init.sh

--- src/main/java/com/vanmors/ndbx/config/CassandraConfig.java
+++ src/main/java/com/vanmors/ndbx/config/CassandraConfig.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import org.springframework.data.cassandra.core.convert.CassandraCustomConversions;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableCassandraRepositories(basePackages = "com.vanmors.ndbx.dao")
@@ -51,16 +49,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     public SchemaAction getSchemaAction() {
-        return SchemaAction.CREATE_IF_NOT_EXISTS;
-    }
-
-    @Override
-    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        return List.of(
-                CreateKeyspaceSpecification.createKeyspace(keyspace)
-                        .ifNotExists()
-                        .withSimpleReplication(1)
-        );
+        return SchemaAction.NONE;
     }
 
     @Override
